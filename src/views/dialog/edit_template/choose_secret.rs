e21d7acf03d4f1cae30c06687cc288f6ebee8ca9
amigin <amigin@gmail.com>
2023-08-31 21:17:23 +0300

    Made Advanced system of adding secret

--- src/views/dialog/edit_template/choose_secret.rs
+++ src/views/dialog/edit_template/choose_secret.rs
@@ -9,55 +9,150 @@ pub struct ChooseSecretProps<'s> {
 
 pub fn choose_secret<'s>(cx: Scope<'s, ChooseSecretProps<'s>>) -> Element {
     let filter = use_state(cx, || "".to_string());
+    let secret_name: &UseState<String> = use_state(cx, || "".to_string());
 
     let secrets: &UseState<Option<Vec<SecretListItem>>> = use_state(cx, || None);
 
-    if secrets.get().is_none() {
-        load_secrets(&cx, secrets);
-    }
+    let mode = use_state(cx, || ChooseSecretMode::Select);
 
-    let values = match secrets.get() {
-        Some(values) => {
-            if filter.get().len() < 3 {
-                vec![]
-            } else {
-                let mut result = Vec::new();
+    let secret_value = use_state(cx, || "".to_string());
 
-                for value in values {
-                    if value.name.to_lowercase().contains(filter.get().as_str()) {
-                        result.push(rsx! {
-                            div {
-                                button {
-                                    class: "btn btn-sm btn-primary",
-                                    onclick: move |_| {
-                                        cx.props.on_selected.call(value.name.to_string());
-                                    },
-                                    "Copy"
-                                }
-                                "{value.name}"
+    let secret_level = use_state(cx, || "".to_string());
+
+    let content = match mode.get() {
+        ChooseSecretMode::Select => {
+            if secrets.get().is_none() {
+                load_secrets(&cx, secrets);
+            }
+
+            match secrets.get() {
+                Some(values) => {
+                    if filter.get().len() < 3 {
+                        vec![]
+                    } else {
+                        let mut result = Vec::new();
+
+                        for value in values {
+                            if value.name.to_lowercase().contains(filter.get().as_str()) {
+                                result.push(rsx! {
+                                    div {
+                                        button {
+                                            class: "btn btn-sm btn-primary",
+                                            onclick: move |_| {
+                                                cx.props.on_selected.call(value.name.to_string());
+                                            },
+                                            "Copy"
+                                        }
+                                        "{value.name}"
+                                    }
+                                })
                             }
-                        })
+                        }
+
+                        result
                     }
                 }
-
-                result
+                None => vec![],
             }
         }
-        None => vec![],
+        ChooseSecretMode::Add => {
+            let btn = if has_secret(&secrets.get(), secret_name.get()) {
+                rsx! { div { class: "alert alert-danger", "Secret already exists" } }
+            } else {
+                rsx! {
+                    button {
+                        class: "btn btn-primary",
+                        onclick: move |_| {
+                            add_secret(
+                                cx,
+                                mode,
+                                secrets,
+                                secret_name.get(),
+                                secret_value.get(),
+                                secret_level.get().parse::<i32>().unwrap(),
+                            );
+                        },
+                        "Add new secret"
+                    }
+                }
+            };
+            let result = rsx! {
+                div { style: "margin-top:10px", class: "form-floating mb-3",
+                    input {
+                        class: "form-control",
+                        value: "{secret_value.get()}",
+                        oninput: move |cx| {
+                            secret_value.set(cx.value.to_string());
+                        }
+                    }
+                    label { "Secret value" }
+                }
+
+                div { class: "form-floating mb-3",
+                    input {
+                        class: "form-control",
+                        r#type: "number",
+                        value: "{secret_level.get()}",
+                        oninput: move |cx| {
+                            secret_level.set(cx.value.to_string());
+                        }
+                    }
+                    label { "Secret level" }
+                }
+
+                btn
+            };
+
+            vec![result]
+        }
+    };
+
+    let btn = match mode.get() {
+        ChooseSecretMode::Select => {
+            rsx! {
+                button {
+                    class: "btn btn-outline-secondary",
+                    style: "width:150px",
+                    onclick: move |_| {
+                        mode.set(ChooseSecretMode::Add);
+                    },
+                    "Select secret"
+                }
+            }
+        }
+        ChooseSecretMode::Add => {
+            rsx! {
+                button {
+                    class: "btn btn-outline-secondary",
+                    style: "width:150px",
+                    onclick: move |_| {
+                        mode.set(ChooseSecretMode::Select);
+                    },
+                    "Add secret"
+                }
+            }
+        }
+    };
+
+    let text = match mode.get() {
+        ChooseSecretMode::Select => "Search secret",
+        ChooseSecretMode::Add => "Add secret",
     };
 
     render! {
-        div { style: "margin-top:5px",
+        div { style: "margin-top:5px; width:100%", class: "input-group",
             input {
                 class: "form-control",
-                placeholder: "Search secret",
+                placeholder: text,
                 oninput: move |cx| {
                     filter.set(cx.value.to_lowercase());
+                    secret_name.set(cx.value.to_string());
                 }
             }
+            btn
         }
 
-        values.into_iter()
+        content.into_iter()
     }
 }
 
@@ -73,4 +168,49 @@ fn load_secrets<'s>(
 
         secrets.set(Some(secrets_values));
     });
+}
+
+fn add_secret<'s>(
+    cx: &Scoped<'s, ChooseSecretProps<'s>>,
+    mode: &UseState<ChooseSecretMode>,
+    secrets: &UseState<Option<Vec<SecretListItem>>>,
+    name: &str,
+    value: &str,
+    level: i32,
+) {
+    let name = name.to_string();
+    let value = value.to_string();
+
+    let mode = mode.to_owned();
+
+    let secrets = secrets.to_owned();
+
+    cx.spawn(async move {
+        crate::grpc_client::SecretsGrpcClient::save_secret(name.clone(), value, level)
+            .await
+            .unwrap();
+
+        mode.set(ChooseSecretMode::Select);
+        secrets.set(None);
+    })
+}
+
+pub enum ChooseSecretMode {
+    Select,
+    Add,
+}
+
+fn has_secret(secrets: &Option<Vec<SecretListItem>>, secret_to_find: &str) -> bool {
+    match secrets {
+        Some(values) => {
+            for value in values {
+                if value.name == secret_to_find {
+                    return true;
+                }
+            }
+        }
+        None => {}
+    }
+
+    false
 }
