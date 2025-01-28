3574ea3d2d777b10e112edb119d4c26a308d5292
amigin <amigin@gmail.com>
2025-01-28 14:57:20 +0200

    Fixes

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let url = "https://raw.githubusercontent.com/MyJetTools/settings-service/main/proto/";
+    let url =
+        "https://raw.githubusercontent.com/MyJetTools/settings-service/refs/heads/main/proto/";
     ci_utils::sync_and_build_proto_file(url, "TemplatesService.proto");
     ci_utils::sync_and_build_proto_file(url, "SecretsService.proto");
     //ci_utils::sync_and_build_proto_file(url, "DomainsService.proto");
