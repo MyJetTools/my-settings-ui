9de8dc38995db323de79dfdb4ef9f2d23039dbd1
amigin <amigin@gmail.com>
2025-01-28 15:41:28 +0200

    Debugging

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 fn main() {
-    let url =
-        "https://raw.githubusercontent.com/MyJetTools/settings-service/refs/heads/main/proto/";
-    ci_utils::sync_and_build_proto_file(url, "TemplatesService.proto");
-    ci_utils::sync_and_build_proto_file(url, "SecretsService.proto");
+    //let url =
+    //    "https://raw.githubusercontent.com/MyJetTools/settings-service/refs/heads/main/proto/";
+    //ci_utils::sync_and_build_proto_file(url, "TemplatesService.proto");
+    //ci_utils::sync_and_build_proto_file(url, "SecretsService.proto");
+
+    ci_utils::tonic_build::compile_protos("./proto/SecretsService.proto").unwrap();
+    ci_utils::tonic_build::compile_protos("./proto/TemplatesService.proto").unwrap();
+
     //ci_utils::sync_and_build_proto_file(url, "DomainsService.proto");
 }
